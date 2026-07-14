ccbdc551edf9b434029af367081e5b77b3bb306c
deevvikasshah <deevvikasshah@gmail.com>
2026-07-15 01:17:54 +0530

    Make Razorpay/Cloudinary configs graceful - allow missing keys

--- server/src/config/razorpay.js
+++ server/src/config/razorpay.js
@@ -1,8 +1,16 @@
 const Razorpay = require("razorpay");
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+const hasRazorpayKeys =
+  process.env.RAZORPAY_KEY_ID &&
+  process.env.RAZORPAY_KEY_ID !== "your_razorpay_key_id" &&
+  process.env.RAZORPAY_KEY_SECRET &&
+  process.env.RAZORPAY_KEY_SECRET !== "your_razorpay_key_secret";
+
+const razorpay = hasRazorpayKeys
+  ? new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    })
+  : null;
 
 module.exports = razorpay;
